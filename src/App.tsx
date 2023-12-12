e6372fab953d108255fae101260eedf700505e3f
mvagner <maksim.vagner@noveogroup.com>
2023-12-12 18:02:24 +0700

    Added basic libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css';
+import logo from './logo.svg';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    ),
-  },
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      }
+    />,
+  ),
+);
 
-const App = () => <RouterProvider router={router} />
+const App = () => <RouterProvider router={router} />;
 
-export default App
+export default App;
