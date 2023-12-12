4b15a9767552e1e422a2a78fcd6fe527043e32ce
mvagner <maksim.vagner@noveogroup.com>
2023-12-12 19:51:09 +0700

    WIP: Adding basic components

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+import { default as BasicButton } from '@mui/material/Button';
+
+const Button: FunctionComponent = () => {
+  return <BasicButton />;
+};
+
+export default Button;
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+import TextField from '@mui/material/TextField';
+
+const TextInput: FunctionComponent = () => {
+  return <TextField />;
+};
+
+export default TextInput;
