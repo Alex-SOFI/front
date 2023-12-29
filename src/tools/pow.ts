be613ca1aa6a3d593bbc8e4b9125a6348e9ba0bb
mvagner <maksim.vagner@noveogroup.com>
2023-12-29 15:48:36 +0700

    Minor fix

--- src/tools/pow.ts
+++ src/tools/pow.ts
@@ -1,6 +1,6 @@
 const pow = (value: string, decimals: number) => {
   const [beforeDot, afterDot] = value.split('.');
-  const decimalsAfterDot = afterDot ? afterDot.length - 1 : 0;
+  const decimalsAfterDot = afterDot ? afterDot.length : 0;
   const formattedValue = afterDot ? beforeDot + afterDot : beforeDot;
 
   return BigInt(formattedValue) * BigInt(10 ** decimals - decimalsAfterDot);
